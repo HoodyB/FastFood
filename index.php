9606079ad5344436020af4cedbc13180fba263a5
HOODY\HP <HoodyB>
2018-04-20 13:22:49 -0500

    Modificación de la hoja de estilos del modelo y estilos de botones en el Login

--- index.php
+++ index.php
@@ -5,18 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index/index.css">
+    <link rel="stylesheet" href="css/index/botonesestilos.css">
+    <link rel="stylesheet" href="css/fuente/fonts.css">
     <title>FastFood: Login</title>
 </head>
 <body>
 <div class="jumbotron" id="div-main">
-<form action="modelo/validarlogin/validacion.php" method="POST">
+<form action="modelo/validarlogin/validar.php" method="POST">
 <h1>FASTFOOD LOGIN</h1>
 <label>Usuario:</label><br>
-<input type="text" name="userr" id="usuario" class="form-control"placeholder="Ingrese aqui" require><br>
+<input type="text" name="userr" id="user" class="form-control"placeholder="Ingrese aqui" require><br>
 <label>Contraseña:</label>
-<input type="password" name="passw" id="paassword" class="form-control" placeholder="Ingrese aqui" require>
-<button type="submit" class="btn btn-primary" id="enviar">Iniciar Sesión</button>
+<input type="password" name="passw" id="pass" class="form-control" placeholder="Ingrese aqui" require>
+<button  class="boton" id="enviar">Iniciar Sesión</button>
+<a href="#"class"boton" >Registrate!</a>
 </form>
 </div>
 </body>
