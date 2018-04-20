395e6f6d9cc332eb4143bf6de758457f6901f409
HOODY\HP <HoodyB>
2018-04-19 21:52:27 -0500

    Modificación de Id de los input y modificación del title y botones

--- index.php
+++ index.php
@@ -6,17 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/index.css">
-    <title>FastFood: Inicio de sesión</title>
+    <title>FastFood: Login</title>
 </head>
 <body>
 <div class="jumbotron" id="div-main">
-<form action="modelo/validarlogin/validar.php" method="POST">
+<form action="modelo/validarlogin/validacion.php" method="POST">
 <h1>FASTFOOD LOGIN</h1>
 <label>Usuario:</label><br>
-<input type="text" name="userr" id="user" class="form-control"placeholder="Ingrese aqui" require><br>
+<input type="text" name="userr" id="usuario" class="form-control"placeholder="Ingrese aqui" require><br>
 <label>Contraseña:</label>
-<input type="password" name="passw" id="pass" class="form-control" placeholder="Ingrese aqui" require>
-<button type="submit" class="btn btn-primary" id="enviar">Iniciar Sesion</button>
+<input type="password" name="passw" id="paassword" class="form-control" placeholder="Ingrese aqui" require>
+<button type="submit" class="btn btn-primary" id="enviar">Iniciar Sesión</button>
 </form>
 </div>
 </body>
